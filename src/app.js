885ec45ca062e6263f570b8eb18ef18314627025
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-02-27 17:42:11 +0530

    Add files via upload
    this back-end done the signup route with token

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:` + PORT);
+  connectDB();
+});
