3a1ad136f5f1db4950d3387f6bd14f5473b3eba1
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-23 22:15:14 +0530

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,7 +12,7 @@ import aiRoutes from "./routes/ai.route.js";
 import { protectRoute } from "./middleware/auth.middleware.js";
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://textspin-chandu.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
