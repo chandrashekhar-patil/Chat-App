7aa6507e1e4b60d255945c5ea674ab5a6273943a
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-24 00:41:56 +0530

    Update axios.js

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -2,7 +2,32 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://chat-app-back-k05j.onrender.com/api",
-  withCredentials: true, // Include cookies in requests
+  withCredentials: true,
 });
+
+// Log request details for debugging
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("Axios request:", {
+      url: config.url,
+      method: config.method,
+      headers: config.headers,
+    });
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log("Axios response error:", {
+      url: error.config?.url,
+      status: error.response?.status,
+      data: error.response?.data,
+    });
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
