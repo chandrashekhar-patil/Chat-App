68451b57ed146496c24900204a8f24652201f840
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-23 22:56:40 +0530

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,14 +5,13 @@ import messageRoutes from "./routes/message.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
 import express from "express";
-import cors from "cors"; // Add this import
 import { app, server } from "./lib/socket.js";
 import groupChatRoutes from "./routes/groupChat.route.js";
 import passwordRoutes from "./routes/password.route.js";
 import aiRoutes from "./routes/ai.route.js";
 import { protectRoute } from "./middleware/auth.middleware.js";
+import cors from "cors";
 
-config({ path: ".env" });
 app.use(cors({
   origin: "https://textspin-chandu.vercel.app",
   credentials: true,
@@ -20,10 +19,13 @@ app.use(cors({
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
 }));
 
+config({ path: ".env" });
 console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
+
 app.use("/uploads/audio", express.static("uploads/audio"));
 
 // Public routes (no authentication required)
