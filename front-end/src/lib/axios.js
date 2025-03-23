37e1ef70174f80680b6ffd11f7845c14598efda2
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-23 22:41:34 +0530

    Update axios.js

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://textspin-chandu-b.vercel.app/api",
   withCredentials: true,
 });
 
