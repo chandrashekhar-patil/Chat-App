5826d615320df8c4bc6fe88ed09811217a52a9de
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-27 22:16:46 +0530

    Update socket.js

--- back-end/src/lib/socket.js
+++ back-end/src/lib/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://textspin-chandu.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   },
