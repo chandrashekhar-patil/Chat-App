1970a6a8e090cad6cf90e6d35a737b80b918d79c
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-27 22:19:41 +0530

    Update auth.route.js

--- back-end/src/routes/auth.route.js
+++ back-end/src/routes/auth.route.js
@@ -1,6 +1,5 @@
-// back-end/src/routes/auth.route.js
 import express from "express";
-import { signup, login, logout, checkAuth ,deleteAccount} from "../controllers/auth.controller.js";
+import { signup, login, logout, checkAuth, deleteAccount, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -10,5 +9,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.get("/check", protectRoute, checkAuth);
 router.delete("/delete-account", protectRoute, deleteAccount);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
